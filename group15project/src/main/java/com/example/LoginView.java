9987744c8c89435e1e03851f9559fe7a4d738ae1
jjarvela23 <168526631+jjarvela23@users.noreply.github.com>
2026-04-24 15:46:23 +0300

    minor additions

--- group15project/src/main/java/com/example/LoginView.java
+++ group15project/src/main/java/com/example/LoginView.java
@@ -16,6 +16,6 @@ public class LoginView extends JFrame{
     JButton backButton;
 
     public LoginView() {
-
+        
     }
 }
--- group15project/src/main/java/com/example/Main.java
+++ group15project/src/main/java/com/example/Main.java
@@ -3,9 +3,11 @@ package com.example;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Main extends JFrame {
 
@@ -13,6 +15,12 @@ public class Main extends JFrame {
     JButton SellButton;
     JLabel label;
     JPanel MainPanel;
+    JTextField searchBar;
+    JComboBox<String> Category;
+    JComboBox<String> Area;
+
+    String[] CateroryList = {"x", "y", "z" };
+    String[] AreaList = {"x", "y", "z" };
 
     public Main() {
         super("MainScreenFrame");
@@ -21,9 +29,12 @@ public class Main extends JFrame {
 
         label = new JLabel();
         label.setText("testing");
+
+        Area = new JComboBox<>(CateroryList);
         MainPanel = new JPanel(new FlowLayout());
 
         MainPanel.add(label);
+        MainPanel.add(Area);
 
         this.getContentPane().add(MainPanel);
     }
