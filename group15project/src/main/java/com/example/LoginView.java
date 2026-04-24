374f5a696c4e0c6b10dc4a8fb58862af9852a72b
jjarvela23 <168526631+jjarvela23@users.noreply.github.com>
2026-04-24 14:57:51 +0300

    Start

---
+++ group15project/src/main/java/com/example/LoginView.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class LoginView extends JFrame{
+
+    JTextField username;
+    JTextField password;
+    JLabel topheader;
+    JLabel usernameText;
+    JLabel passwordText;
+    JButton loginButton;
+    JButton backButton;
+
+    public LoginView() {
+
+    }
+}
--- group15project/src/main/java/com/example/Main.java
+++ group15project/src/main/java/com/example/Main.java
@@ -1,7 +1,36 @@
 package com.example;
 
-public class Main {
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main extends JFrame {
+
+    JButton UserButton;
+    JButton SellButton;
+    JLabel label;
+    JPanel MainPanel;
+
+    public Main() {
+        super("MainScreenFrame");
+
+        setSize(1980, 1080);
+
+        label = new JLabel();
+        label.setText("testing");
+        MainPanel = new JPanel(new FlowLayout());
+
+        MainPanel.add(label);
+
+        this.getContentPane().add(MainPanel);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main myprogram = new Main();
+        myprogram.setVisible(true);
     }
 }
